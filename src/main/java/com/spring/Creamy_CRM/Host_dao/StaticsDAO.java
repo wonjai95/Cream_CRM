9cf031d4f72583805c89ce7ecf28a93d54dce575
sky2000sh <sky2000sh@gmail.com>
2021-10-12 02:45:47 +0900

    [하기 예약관련 & 통계관련 파일들 수정] 1. 고객페이지에서 예약하기 진행 시 결제페이지에서는 총 가격이 insert되는 반면, 예약페이지에서는 총 가격이 insert되지 않는 점을 발견 -> 수정 완료 . UserReservationServiceImpl
    2. 관리자페이지에서
    2-1) 통계 - 판매통계 - 유형(호실)별 판매현황:-
    가. 호실별 판매 목록 보이기 완료
    나. 호실별 판매 차트 보이기 1차 완료 -> 2차 응용심화 진행 중
    (차트부분에서는 월별 각 호실의 매출이 정확히 나오는지 더 고심할 필요)
    . StaticsController
    . StaticsServiceImpl
    . StaticsService
    . StaticsDAOImpl
    . StaticsDAO
    . StaticVO
    . statics_sqlMapper.xml
    . salesStatics.js
    . salesStatics.jsp
    . roomSalesStatics.jsp 추가

--- src/main/java/com/spring/Creamy_CRM/Host_dao/StaticsDAO.java
+++ src/main/java/com/spring/Creamy_CRM/Host_dao/StaticsDAO.java
@@ -13,9 +13,18 @@ import com.spring.Creamy_CRM.VO.StaticVO;
 
 public interface StaticsDAO {
 
+	// host_code를 이용한 employee_code 가져오기
+	public List<StaticVO> getEmpCode(String host_code);
+	
+	// host_code를 이용한 room_setting_code 가져오기
+	public List<StaticVO> getRoomCode(String host_code);
+	
 	// 직원(담당자)별 판매정보 목록 조회
 	public List<StaticVO> managerSalesList(String host_code);
 	
 	// 직원(담당자)별 판매정보 차트 조회
 	public List<StaticVO> managerSalesChart(String employee_code);
+	
+	// 유형(호실)별 판매정보 목록 조회
+	public List<StaticVO> roomSalesList(String host_code);
 }
--- src/main/webapp/resources/host/js/salesStatics.js
+++ src/main/webapp/resources/host/js/salesStatics.js
@@ -28,4 +28,34 @@ $("document").ready(function() {
       });
    });
    
+   
+   // 3번 탭 클릭시 ajax 발동
+   $("a[href='#tab-3']").click(function() {
+	  console.log("탭3 클릭");
+	  
+	  // var header = 'X-CSRF-TOKEN' 아래 방식이 안 될 경우 이거 사용
+	  var header = $("meta[name='_csrf_header']").attr("content");
+      var token = $("meta[name='_csrf']").attr("content");
+      console.log("header : " + header);
+      console.log("token : " + token);
+
+      $.ajax({
+         url : "roomSalesStatics",
+         type : "Post",
+         beforeSend : function(jqXHR, settings) {
+            console.log("beforesend 진행");
+            jqXHR.setRequestHeader(header, token);
+         },
+         success : function(result) {
+            $(".tab-content").html(result);
+         },
+         error : function(error) {
+        	alert("다시 시도해주세요.");
+        	console.log("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+            window.history.back();
+         }
+         
+      });
+   });
+   
 });
