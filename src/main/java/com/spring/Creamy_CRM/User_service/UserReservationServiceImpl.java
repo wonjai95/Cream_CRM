5bc3bc50b6f4f74de3825d25af8133e644485127
won <wonje1335@gmail.com>
2021-10-15 11:57:03 +0900

    수림이 수정

--- src/main/java/com/spring/Creamy_CRM/User_service/UserReservationServiceImpl.java
+++ src/main/java/com/spring/Creamy_CRM/User_service/UserReservationServiceImpl.java
@@ -345,11 +345,16 @@ public class UserReservationServiceImpl implements UserReservationService {
 				
 			// 예약 시작 시간 < 예약 종료시간
 			}  else {
+				
+				if(Integer.parseInt(res_start) < 10) res_start = "0" + res_start;
+	            if(Integer.parseInt(res_end) < 10) res_end = "0" + res_end;
+				
 				res_start = res_start + ":00";
 				res_end = res_end + ":00";
 				vo.setRes_start(res_start);
 				vo.setRes_end(res_end);
-				
+
+			
 				// 예약 신청 시간 가능 여부 체크를 위한 뷰 생성를 생성해서, 그 뷰와 reservation_tbl를 조인
 				// => count(*) = 0이면 예약 가능
 				// 예약 신청 시간 가능 여부 체크를 위한 뷰 생성 sql문
